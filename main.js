53f22fdfe59b36c0a39d524ff4a15f87966b2679
Himanshu Sachan <46996877+Sachanh@users.noreply.github.com>
2020-12-31 12:54:39 +0530

    uploaded necessary

---
+++ main.js
@@ -0,0 +1,16 @@
+var section =document.querySelector(".sec");
+var innerdiv =document.querySelector(".innerdiv")
+var array =["milk", "chocolat","pen","paper","broom"];
+var btn = document.querySelector("button");
+btn.addEventListener('click',grocery )
+function grocery(){
+    var ar=[];
+    for(var i=0;i<array.length;i++)
+   if(i%2==0) {
+      
+       innerdiv.innerText=array[i];
+       console.log(array[i])
+
+    }
+
+}
